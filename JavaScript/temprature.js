58629f4a8e0176c3ffa3d7506cb4207624022f51
Jay Darji <jaydarji369@gmail.com>
2020-06-01 01:14:02 -0400

    Java Script Learning

---
+++ JavaScript/temprature.js
@@ -0,0 +1,13 @@
+// Defining constant variable with value as 293
+const kelvin = 0;
+
+//celcious is 273 less thank
+var celcious = kelvin - 273;
+
+//calculating fahrenheit
+let fahrenheit = Math.floor(celcious * (9/5) + 32);
+
+var newton = celcious * (33/100);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+
