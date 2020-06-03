d1aaadd5a2e3dbd1c494ff4a89622b2a75ae60b9
Jay Darji <jaydarji369@gmail.com>
2020-06-03 16:52:48 -0400

    Learning JS

--- JavaScript/Function.js
+++ JavaScript/Function.js
@@ -14,3 +14,38 @@ function sayThanks(){
 sayThanks();
 sayThanks();
 sayThanks();
+
+
+function sayThanks(name) {
+    console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
+}
+
+sayThanks('Cole');
+
+
+
+
+// assigning default values to the parameter of the functions
+
+function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
+    console.log(`Remember to buy ${item1}`);
+    console.log(`Remember to buy ${item2}`);
+    console.log(`Remember to buy ${item3}`);
+}
+
+// return
+
+function rectangleArea(width, height) {
+    if (width < 0 || height < 0) {
+        return 'You need positive integers to calculate area!';
+    }
+    return width * height;
+}
+
+function monitorCount(rows, columns){
+    return rows * columns;
+}
+
+const numOfMonitors = monitorCount(5,4);
+
+console.log(numOfMonitors);
