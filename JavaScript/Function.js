0fd5aa3a0a761e4ae5ffdf6721c44bd6ae6e62f9
Jay Darji <jaydarji369@gmail.com>
2020-06-03 17:02:35 -0400

    Learning JS

--- JavaScript/Function.js
+++ JavaScript/Function.js
@@ -49,3 +49,31 @@ function monitorCount(rows, columns){
 const numOfMonitors = monitorCount(5,4);
 
 console.log(numOfMonitors);
+
+
+// call function from another function
+function monitorCount(rows, columns) {
+    return rows * columns;
+}
+
+function costOfMonitors(rows, columns){
+    return monitorCount(rows, columns) * 200;
+}
+
+const totalCost = costOfMonitors(5,4);
+console.log(totalCost);
+
+
+
+// Function Expression
+
+const plantNeedsWater = function(day){
+    if(day === 'Wednesday'){
+        return true;
+    }else{
+        return false;
+    }
+};
+
+console.log(plantNeedsWater('Tuesday')); // false
+console.log(plantNeedsWater); // [Function: plantNeedsWater]
