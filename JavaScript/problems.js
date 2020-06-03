7fc179b254189e555292c91344a90e7e58e2e53b
Jay Darji <jaydarji369@gmail.com>
2020-06-03 19:41:04 -0400

    Learning JS

---
+++ JavaScript/problems.js
@@ -0,0 +1,31 @@
+var canIVote = age => (age >= 18)? true : false;
+
+// Uncomment the line below when you're ready to try out your function
+console.log(canIVote(19)) // Should print true
+
+// We encourage you to add more function calls of your own to test your code!
+
+
+
+//-------------------------------------------------------
+
+// Write your function here:
+
+function agreeOrDisagree(str1, str2){
+    if( str1 === str2){
+        return 'You agree!';
+    }else{
+        return 'You disagree!';
+    }
+}
+
+
+
+
+// Uncomment the line below when you're ready to try out your function
+console.log(agreeOrDisagree("yep", "yep"))
+// Should print 'You agree!'
+
+// We encourage you to add more function call of your own to test your code!
+
+//-------------------------------------------------------
