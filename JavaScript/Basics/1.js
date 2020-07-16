1cf12618aa558499258949a87ede871ae2d4ffcf
Jay Darji <jaydarji369@gmail.com>
2020-07-16 00:43:59 -0400

    Learning basics of JS

--- JavaScript/Basics/1.js
+++ JavaScript/Basics/1.js
@@ -9,3 +9,17 @@ function logCitySkyline(){
 console.log(logCitySkyline())
 
 //You’ll notice that the logCitySkyline() function is able to access both variables without any problems
+
+//
+
+let satellite = 'The Moon'
+let galaxy = 'The Milky Way'
+let stars = 'North Star'
+
+function callMyNightSky(){
+    return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+}
+
+console.log(callMyNightSky())
+//
+//
