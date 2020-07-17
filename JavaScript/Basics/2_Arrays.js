5b87aabae198c50304abafcc70d65fd9307793bf
Jay Darji <jaydarji369@gmail.com>
2020-07-16 22:15:06 -0400

    Learning basics of JS

--- JavaScript/Basics/2_Arrays.js
+++ JavaScript/Basics/2_Arrays.js
@@ -9,3 +9,14 @@ console.log(newYearsResolutions);
 const hobbies = ['Jay','Ajay','Vijay']
 
 console.log(hobbies)
+
+
+//
+
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+let listItem = famousSayings[0]
+console.log(listItem)
+console.log(famousSayings[2])
+console.log(famousSayings[2][0])
+console.log(famousSayings[3])
