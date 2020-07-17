ec6344535744d95ad59966d84ad83a33612481f9
Jay Darji <jaydarji369@gmail.com>
2020-07-16 23:29:49 -0400

    Learning basics of JS

--- JavaScript/Basics/3_Loop.js
+++ JavaScript/Basics/3_Loop.js
@@ -59,4 +59,31 @@ while(currentCard != 'spade'){
 }
 
 
+//
+
+// Write your code below
+
+let cupsOfSugarNeeded = 3
+let cupsAdded = 0
+
+do{
+    cupsAdded++
+}while (cupsAdded < cupsOfSugarNeeded)
+
+//
+
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+// Write your code below
+
+for(let i = 0; i < rapperArray.length; i++){
+    console.log(rapperArray[i])
+    if(rapperArray[i] === 'Notorious B.I.G.'){
+        break
+    }
+
+}
+
+console.log("And if you don't know, now you know.")
+
 
