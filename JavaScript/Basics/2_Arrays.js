fd76f3591a48eb9e50c033a23eb6376b7d581d0b
Jay Darji <jaydarji369@gmail.com>
2020-07-16 22:10:43 -0400

    Learning basics of JS

---
+++ JavaScript/Basics/2_Arrays.js
@@ -0,0 +1,11 @@
+// Arrays
+
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
+
+console.log(newYearsResolutions);
+
+//
+
+const hobbies = ['Jay','Ajay','Vijay']
+
+console.log(hobbies)
