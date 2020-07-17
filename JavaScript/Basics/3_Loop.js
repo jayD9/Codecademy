9ab4a72b8de2c8e2cc1ad9d40be2850c40afb458
Jay Darji <jaydarji369@gmail.com>
2020-07-16 23:13:14 -0400

    Learning basics of JS

--- JavaScript/Basics/3_Loop.js
+++ JavaScript/Basics/3_Loop.js
@@ -5,3 +5,24 @@ console.log(vacationSpots[0])
 console.log(vacationSpots[1])
 console.log(vacationSpots[2])
 
+//
+// Write your code below
+for(let count = 5; count < 11; count++){
+    console.log(count)
+}
+
+//
+
+// The loop below loops from 0 to 3. Edit it to loop backwards from 3 to 0
+for (let counter = 3; counter >= 0; counter--){
+    console.log(counter);
+}
+
+//
+
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+// Write your code below
+for(let i = 0 ; i < vacationSpots.length; i++){
+    console.log('I would love to visit '+vacationSpots[i])
+}
