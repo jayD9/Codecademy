203a5e158a1a6f1af34e8a708376248a56b8c60f
Jay Darji <jaydarji369@gmail.com>
2020-07-16 22:22:13 -0400

    Learning basics of JS

--- JavaScript/Basics/2_Arrays.js
+++ JavaScript/Basics/2_Arrays.js
@@ -20,3 +20,23 @@ console.log(listItem)
 console.log(famousSayings[2])
 console.log(famousSayings[2][0])
 console.log(famousSayings[3])
+
+
+//
+
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+
+condiments[0] = 'Mayo'
+console.log(condiments)
+
+condiments = ['Mayo']
+console.log(condiments)
+
+utensils[3] = 'Spoon'
+console.log(utensils)
+
+utensils = ['Spoon']
+console.log(utensils)
